59152d09a1c2b795b320eae730475f15b2273ed6
gabrielafariac <126210298+gabrielafariac@users.noreply.github.com>
2025-07-31 00:24:00 +0000

    final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  const myButton1  = document.querySelector('.mybutton')
+  const card = document.querySelector('.card')
+
+  let generateRandomNumber = () =>{
+    
+    let numbers =["A","2", "3","4","5","6","7","8","9","J","Q","K"];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+  };
+
+  let generateRandomSuit = () => {
+    let suit = ["diams","spade","hearts","clubs"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    return suit[indexSuit];
+  };
+
+  let generatedCard =() => {
+    card.className="card";
+    let suit =generateRandomSuit ();
+    card.classList.add(suit);
+    card.innerHTML = generateRandomNumber ();
+
+  };
+  
+window.onload = generatedCard;
+myButton1.addEventListener("click", generatedCard);
